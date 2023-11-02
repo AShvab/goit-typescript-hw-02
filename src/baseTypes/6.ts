3ed878551a8fb70accfa4f97ea4d82db0458c609
AShvab <shvabovska@gmail.com>
2023-11-03 00:20:38 +0200

    finish baseTypes

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,18 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+// приймає один параметр — message типу string і нічого не повертає, тому тип, що повертається — void:
+function showMessage(message: string): void {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-function customError() {
-  throw new Error('Error');
+// Never - коли функція ніколи не закінчується або нічого не повертає
+function customError(): never {
+  throw new Error("Error");
 }
 
 export {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,23 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayInWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+ }
+
+ function isWeekend(day: DayInWeek): boolean {
+  return day === DayInWeek.Saturday || day === DayInWeek.Sunday;
+ }
+
+ const currentDay: DayInWeek = DayInWeek.Monday;
+ const isCurrentDayWeekend: boolean = isWeekend(currentDay);
+ 
+ console.log(`Is it a Weekend?: ${isCurrentDayWeekend}`)
+
